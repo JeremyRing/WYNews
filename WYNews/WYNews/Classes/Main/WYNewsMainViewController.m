1f1c4afebf48c0143415359f2061cab66b8091e2
刘杰 <liujie_998198@163.com>
2017-07-25 10:22:43 +0800

    add MainController

---
+++ WYNews/WYNews/Classes/Main/WYNewsMainViewController.m
@@ -0,0 +1,37 @@
+//
+//  WYNewsMainViewController.m
+//  WYNews
+//
+//  Created by 刘杰 on 2017/7/25.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import "WYNewsMainViewController.h"
+
+@interface WYNewsMainViewController ()
+
+@end
+
+@implementation WYNewsMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
